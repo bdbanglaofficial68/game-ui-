30d3d1699c1e5b8569c81527d9b11df9efd2a0d9
bdbanglaofficial68 <bdbanglaofficial68@gmail.com>
2026-07-10 00:00:48 +0600

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,34 @@
+const path = require("path");
 
+module.exports = {
+  mode: "development",
+  entry: "./js/index.js",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"]
+          }
+        }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  devServer: {
+    static: "./",
+    port: 8080,
+    open: true
+  }
+};
